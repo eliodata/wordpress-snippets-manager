e42c15b3c75820da418e3d3dbf045ca8caa596f1
Elio Data <elio@example.com>
2025-07-11 09:22:23 +0200

    Restructure repository: move extension to root, remove user data files

--- .snippet_cache/snippet-146.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Snippet ID: 146
- * Name: 
- * Description: Ceci est une description pour le snippet 146.
- * @active false
- */
-
-// Ajoutez votre code ici
--- .snippet_cache/snippet-55.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Snippet ID: 55
- * Name: afficher meta cpt formateur 505
- * Description: Ceci est une description pour le snippet 55.
- * @active false
- */
-
-// Ajoutez votre code ici
